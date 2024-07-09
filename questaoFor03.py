55cc7039436e827bef309df546858a416f8f9039
JG1511 <joaotsi2024@gmail>
2024-07-09 18:41:26 -0300

    Questão05

--- questaoFor03.py
+++ questaoFor03.py
@@ -1,6 +1,8 @@
 #Questão 03
-var_1 = int(input("Digite o pireimeiro valor inteiro: "))
-var_2 = int(input("Digite o segundo valor inteiro:"))
+var_1 = int(input("Digite o pireimeiro valor inteiro positivo: "))
+var_2 = int(input("Digite o segundo valor inteiro positivo:"))
 
 for i in range(var_1, var_2 + 1):
     print(i, end = ",")
+
+print("Fim do Programa")
