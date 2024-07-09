426da66402be56eb7181d68cadbb96ead041be8d
RobertMN076 <robert.giulio@escolar.ifrn.edu.br>
2024-07-09 18:16:20 -0300

    questao04

--- questao04.py
+++ questao04.py
@@ -1,7 +1,6 @@
 num1 = int(input('Digite o numero:'))
 num2 = int(input('Digite o segundo numero:'))
 
-
-for i in range(num1, num2):
+for i in range(num1+1, num2):
     print(i)
 print('Programa encerrado')
