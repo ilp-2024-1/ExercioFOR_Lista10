93405aedaff6212c3f0a823913c7eeebb485f20e
JG1511 <joaotsi2024@gmail>
2024-07-09 17:53:51 -0300

    questao03

---
+++ questaoFor03.py
@@ -0,0 +1,6 @@
+#Questão 03
+var_1 = int(input("Digite o pireimeiro valor inteiro: "))
+var_2 = int(input("Digite o segundo valor inteiro:"))
+
+for i in range(var_1, var_2 + 1):
+    print(i, end = ",")
